a32d5890e891c62f0064247eec7ea0d39780817e
aboualeey <hajjialiyu.abubakar@gmail.com>
2025-08-22 21:29:41 +0100

    Add admin layout and services components

---
+++ spherical-gis/src/components/admin/services/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export default function ServicesManagement() {
+  // Services CRUD functionality
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl font-bold">Services Management</h1>
+      {/* Services management interface */}
+    </div>
+  )
+}
